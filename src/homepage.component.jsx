2000ec64e03f4faa6689ece2424090b6e6325b64
wenaro <enake95@gmail.com>
2021-12-28 12:11:53 +0100

    Initial menu structure, publish it to GH pages

---
+++ src/homepage.component.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './homepage.styles.css';
+
+const HomePage = () => {
+    return (
+        <div className='homepage'>
+            <div className='directory-menu'>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>HATS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>JACTES</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>SNEACKERS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>WOMANS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>MANS</h1>
+                        <span className='subtitle'>SHOP NOW</span>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        
+        
+    )
+}
+
+export default HomePage;
