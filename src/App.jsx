1dbb3b87af83bbf1b2458bad3037ca77b0c90d5a
wenaro <enake95@gmail.com>
2022-01-06 14:15:49 +0100

    Adding header component with home and shop navigation

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Header from "./components/header/header";
+import HomePage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shop";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />{" "}
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>{" "}
+    </div>
+  );
+}
+
+export default App;
