a3cd7555750ca6c6d2ff7e406e8cd0aa132cdb33
Sebastian Podgorski <S.Podgorski@adbglobal.com>
2022-01-19 15:28:05 +0100

    added reselect liibrary to skip rerender components which does not change due to returning allways new state in reducers

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -2,18 +2,23 @@ import React from "react";
 import { connect } from "react-redux";
 import { ReactComponent as Shoppingicon } from "../../assets/shopping-bag.svg";
 import { toogleCartHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 import "./cart-icon.css";
 
 
-const CartIcon = ({ toogleCartHidden }) => (
+const CartIcon = ({ toogleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toogleCartHidden}>
     <Shoppingicon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
+
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state)
+})
 
 const mapDispatchToProps = (dispatch) => ({
   toogleCartHidden: () => dispatch(toogleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
