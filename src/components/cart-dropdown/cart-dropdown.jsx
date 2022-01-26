5862c57743cf2844a74f4e0f75c6873fd37111f2
Sebastian Podgorski <S.Podgorski@adbglobal.com>
2022-01-26 15:34:04 +0100

    improve user expirence by hidding cart dropdown when clicking checkout

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -3,12 +3,13 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toogleCartHidden } from '../../redux/cart/cart.actions';
 import CartItem from '../cart-item/cart-item';
 import CustomButton from '../custom-button/custom-button';
 import './cart-dropdown.css'
 
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             {
@@ -17,12 +18,16 @@ const CartDropdown = ({ cartItems, history }) => (
                     : <span className='empty-message'>Your cart is empty</span>
             }
         </div>
-        <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={() => {
+            history.push('/checkout');
+            dispatch(toogleCartHidden());
+        }}>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
 const mapStateToProps = createStructuredSelector({
     cartItems: selectCartItems
 })
+
 
 export default withRouter(connect(mapStateToProps)(CartDropdown));
